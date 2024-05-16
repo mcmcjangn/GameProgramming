1fd15c2f378a4e38c3c6a9898547d8efb233c7ae
mcmc <153211029+mcmcjangn@users.noreply.github.com>
2024-05-16 14:39:22 +0900

    Add files via upload

---
+++ AIRPLANE/소스코드/GameFunc.cpp
@@ -0,0 +1,223 @@
+#include "GameFunc.h"
+#include <windows.h>
+
+int g_input;
+std::string g_output;
+
+int g_X;
+int g_Y;
+
+// 흘러간 시간 기록
+double g_elapsed_time_ms;
+
+//미사일 발사 여부
+bool g_flag_missile;
+
+//미사일 위치
+int g_missile_X;
+int g_missile_Y;
+std::string g_missile_output;
+
+/////////////////////////////////////////////////////////////
+// InitGame() 
+// 프로그램이 시작될 때 최초에 한 번 호출되는 함수.
+// 이 함수에서 게임에 필요한 자원(이미지, 사운드 등)을 로딩하고, 상태 변수들을 초기화 해야한다.
+void InitGame() {
+	g_input = 0;
+	g_output = "*";
+	g_flag_running = true;
+
+	g_missile_output = "!";
+	g_flag_missile = false;
+
+	g_X = 0;
+	g_Y = 10;
+	g_elapsed_time_ms = 0;
+
+	// std::cout 출력에 버퍼를 사용하여, 출력 속도가 느려지는 현상을 피한다.
+	setvbuf(stdout, NULL, _IOLBF, 4096);
+
+	// Clear the console screen.
+	// 표준출력 화면을 깨끗히 지운다.
+	system("cls");
+}
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+// Update() 
+// 게임의 내용을 업데이트하는 함수.
+// 실제 게임의 룰을 구현해야하는 곳.
+// 게임에서 일어나는 변화는 모두 이 곳에서 구현한다.
+// main 함수의 while loop에 의해서 무한히 반복 호출된다는 것을 주의.
+void Update() {
+	//좌측 이동
+	if (g_input == 1) {
+		g_X-=1;
+		if (g_X < 0) {
+			g_X = 0;
+		}
+	}
+	//우측 이동
+	else if (g_input == 2) {
+		g_X+=1;
+		if (g_X > 29) {
+			g_X = 29;
+		}
+	}
+	//위로 이동
+	else if (g_input == 4) {
+		g_Y -= 1;
+		if (g_Y < 0) {
+			g_Y = 0;
+		}
+	}
+	//아래로 이동
+	else if (g_input == 5) {
+		g_Y += 1;
+		if (g_Y > 19) {
+			g_Y = 19;
+		}
+	}
+	//미사일 발사
+	else if (g_input == 3) {
+		if (g_flag_missile == false) {
+			g_flag_missile = true;
+			g_missile_Y = g_Y;
+			g_missile_X = g_X;
+		}
+	}
+
+	//미사일 이동
+	if (g_flag_missile == true) {
+		g_missile_Y -= 1;
+		if (g_missile_Y < 0) {
+			g_flag_missile = false;
+		}
+	}
+	g_elapsed_time_ms += 33;
+}
+
+
+
+
+/////////////////////////////////////////////////////////////
+// Render() 
+// 그림을 그리는 함수.
+// main 함수의 while loop에 의해서 무한히 반복 호출된다는 것을 주의.
+void Render() {
+	//// 1. 배경 그리기.
+	// 1.1. 커서를 콘솔 화면의 왼쪽 위 모서리 부분으로 옮긴다. 좌표(0, 0)
+	// <windows.h>에서 제공하는 함수를 사용한다.
+	COORD cur;
+	cur.X = 0;
+	cur.Y = 0;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
+
+	COORD cur_missile;
+	cur_missile.X = 0;
+	cur_missile.Y = 0;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur_missile);
+
+	//// 1.2. 배경 부분을 '_'으로 채운다.
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 30; j++) {
+			std::cout << "_";
+		}
+		std::cout << std::endl;
+	}
+
+	//// 1.3. 배경 아래에 시간을 표시한다,
+	std::cout << "Elapsed Time: " << g_elapsed_time_ms / 1000.0f << std::endl;
+
+
+	//// 2. 캐릭터 그리기.
+	// 2.1. 커서를 캐릭터가 그려질 위치로 옮긴다. 
+	cur.X = g_X;
+	cur.Y = g_Y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
+
+	// 2.2. 캐릭터 표정을 그린다.
+	std::cout << g_output;
+
+
+	std::cout.flush();
+
+	//미사일 그리기
+	if (g_flag_missile == true) {
+		cur_missile.X = g_missile_X;
+		cur_missile.Y = g_missile_Y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur_missile);
+		std::cout << g_missile_output;
+	}
+
+	//// 3. 프레임 완성.
+	// std::cout으로 출력한 내용 중, 아직 화면에 표시되 않고 버퍼에 남아 있는 것이 있다면, 모두 화면에 출력되도록 한다.
+	std::cout.flush();
+}
+
+
+
+/////////////////////////////////////////////////////////////
+// HandleEvents() 
+// 이벤트를 처리하는 함수.
+// main 함수의 while loop에 의해서 무한히 반복 호출된다는 것을 주의.
+void HandleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event)) {
+		switch (event.type) {
+
+		case SDL_QUIT:
+			g_flag_running = false;
+			break;
+
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_LEFT) {
+				g_input = 1;
+			}
+			else if (event.key.keysym.sym == SDLK_RIGHT) {
+				g_input = 2;
+			}
+			else if (event.key.keysym.sym == SDLK_SPACE) {
+				g_input = 3;
+			}
+			else if (event.key.keysym.sym == SDLK_UP) {
+				g_input = 4;
+			}
+			else if (event.key.keysym.sym == SDLK_DOWN) {
+				g_input = 5;
+			}
+			break;
+
+		case SDL_KEYUP:
+			g_input = 0;
+			break;
+
+		case SDL_MOUSEBUTTONDOWN:
+
+			// 마우스 왼쪽 버튼이 눌려 졌을 때.
+			if (event.button.button == SDL_BUTTON_LEFT) {
+				g_input = 0;
+			}
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
+
+
+
+/////////////////////////////////////////////////////////////
+// ClearGame() 
+// 프로그램이 끝날 때 한 번 호출되는 함수.
+// 이 함수에서 사용된 자원(이미지, 사운드 등)과 메모리 등을 해제해야한다.
+void ClearGame()
+{
+}
